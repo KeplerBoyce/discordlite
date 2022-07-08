6b7b70a5dd09399b9ec27fc9d041f01aa5596924
KeplerBoyce <keplerboy@gmail.com>
2022-07-08 11:44:51 -0700

    fix authentication

--- src/util/api.ts
+++ src/util/api.ts
@@ -2,7 +2,7 @@ export function getMessages(channelId: string) {
     return fetch('https://discord.com/api/v9/channels/' + channelId + '/messages?limit=20', {
         method: 'GET',
         headers: {
-            authorization: 'NTgxNjgyNjExMTk0MTY3MzA2.GloLcI.aAenDAPDCZ27KrAtH0jJLipYuy2YFUZUsfhoNQ',
+            authorization: '' + process.env.REACT_APP_DISCORD_AUTH,
         },
     }).then(res => res.json())
 }
